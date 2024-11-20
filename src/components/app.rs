40131ca054da9e626ad776887612a8c48f027cbb
misut <pkr5207@gmail.com>
2024-11-21 07:31:35 +0900

    style: format codes

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,20 +1,29 @@
-use components::entrance::Entrance;
 use dioxus::prelude::*;
 
 #[component]
 pub fn App() -> Element {
     // Build cool things ✌️
-
-    Entrance();
     rsx! {
-        link { rel: "stylesheet", href: "main.css" }
-        img { src: "header.svg", id: "header" }
+        link { rel: "stylesheet", href: manganis::mg!(file("./assets/main.css")) }
+        img { src: manganis::mg!(file("./assets/header.svg")), id: "header" }
         div { id: "links",
             a { target: "_blank", href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
             a { target: "_blank", href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-            a { target: "_blank", href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-            a { target: "_blank", href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
-            a { target: "_blank", href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+            a {
+                target: "_blank",
+                href: "https://github.com/dioxus-community/",
+                "📡 Community Libraries"
+            }
+            a {
+                target: "_blank",
+                href: "https://github.com/DioxusLabs/dioxus-std",
+                "⚙️ Dioxus Standard Library"
+            }
+            a {
+                target: "_blank",
+                href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
+                "💫 VSCode Extension"
+            }
             a { target: "_blank", href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
         }
     }
