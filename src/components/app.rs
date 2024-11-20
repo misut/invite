d1e98e2a188f27d1176989bdc73b4d018eba4526
misut <pkr5207@gmail.com>
2024-11-20 14:53:07 +0900

    refactor: separate mods

---
+++ src/components/app.rs
@@ -0,0 +1,19 @@
+use dioxus::prelude::*;
+
+#[component]
+pub fn App() -> Element {
+    // Build cool things ✌️
+
+    rsx! {
+        link { rel: "stylesheet", href: "main.css" }
+        img { src: "header.svg", id: "header" }
+        div { id: "links",
+            a { target: "_blank", href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
+            a { target: "_blank", href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+            a { target: "_blank", href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+            a { target: "_blank", href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
+            a { target: "_blank", href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+            a { target: "_blank", href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+        }
+    }
+}
