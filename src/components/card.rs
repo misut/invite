ff68c32c3d71dc9c46586b72bb0b606a78fd5e32
misut <36529239+misut@users.noreply.github.com>
2024-12-02 08:10:40 +0900

    feat: 20241202 (#4)
    * feat: snowflakes disappear smoothly
    
    * feat: separate by sections

--- src/components/card.rs
+++ src/components/card.rs
@@ -6,15 +6,25 @@ pub fn Card() -> Element {
             script { src: "./snowflake.js" }
             img {
                 id: "snowflake",
+                alt: "Snowflake",
                 class: "margin-large",
                 src: "./snowflake.svg",
                 style: "width: 20vh; height: 20vh"
             }
             p { class: "font-title margin-large", "송년회 초대장" }
+            p { class: "font-body margin-small",
+                "2024년 광고서버개발플랫폼 송년회에 초대합니다"
+            }
+
+            div { class: "horizontal-line", aria_hidden: true }
+            p { class: "font-subtitle margin-medium", "날짜 및 위치" }
             p { class: "font-body margin-small", "2024년 12월 18일 18:00" }
             p { class: "font-body margin-small", "경기도 성남시 분당구 서현로357번길 6" }
             p { class: "font-comment margin-small", "* 주차는 할 수 없어요" }
             Map {}
+
+            div { class: "horizontal-line", aria_hidden: true }
+            p { class: "font-subtitle margin-medium", "준비물" }
         }
     }
 }
